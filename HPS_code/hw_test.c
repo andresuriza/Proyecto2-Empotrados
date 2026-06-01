622f892c211322a63dc13b4ec22074a4ad7133ad
andresuriza <85364572+andresuriza@users.noreply.github.com>
2026-05-31 19:50:47 -0600

    Prueba polling funcional

--- HPS_code/hw_test.c
+++ HPS_code/hw_test.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include <fcntl.h>
-#include <sys/mman.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <sys/mman.h>
 #include <unistd.h>
-
-#define LW_BRIDGE_BASE 0xFF200000
-#define MAP_SIZE       0x40000
 
 #define HEX_HI_OFFSET  0x31040
 #define HEX_LO_OFFSET  0x31050
+
+#define LW_BRIDGE_BASE 0xFF200000
+#define MAILBOX_BASE 0xF000
+#define MAP_SIZE       0x40000
 
 #define SHARED_OFFSET 0x00000
 
@@ -16,17 +17,11 @@ int main()
 {
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
 
-    volatile uint32_t *lw =
-        mmap(NULL, MAP_SIZE,
-             PROT_READ | PROT_WRITE,
-             MAP_SHARED,
-             fd,
-             LW_BRIDGE_BASE);
+    volatile uint32_t *lw = mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, LW_BRIDGE_BASE);
+    volatile uint32_t *mailbox = (volatile uint32_t*) ((char*) lw + MAILBOX_BASE);
 
-    volatile uint32_t *shared =
-    (volatile uint32_t *)((char*)lw);
-
-    shared[0x1000/4] = 0x12345678;
+    mailbox[0] = 1;   // PLAY
+    mailbox[1] = 7;   // Track 7
 
     return 0;
 }
